a1aa861c81e5fa18f2053660382b33a50a95e6bf
Adrian D <adi.dancau@gmail.com>
2021-11-17 21:12:54 +0000

    added calendar 2.2

--- app/src/main/java/com/example/forfoodiesbyfoodies/Views/BookTable.java
+++ app/src/main/java/com/example/forfoodiesbyfoodies/Views/BookTable.java
@@ -2,19 +2,21 @@ package com.example.forfoodiesbyfoodies.Views;
 
 import androidx.annotation.NonNull;
 
-import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.CalendarView;
-import com.example.forfoodiesbyfoodies.R;
-import android.widget.Toast;
 
+import com.example.forfoodiesbyfoodies.R;
+
+import android.widget.Toast;
 
 
 public class BookTable extends AppCompatActivity {
@@ -36,16 +38,16 @@ public class BookTable extends AppCompatActivity {
         //Add listener in calendar with a method to get the value of days,month and years
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
-            @RequiresApi(api = Build.VERSION_CODES.N)
+            //TODO - change the position for the toast message on top of the "Book a table" button
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                Toast toast =  Toast.makeText(getApplicationContext(),
-                        "Selected Date: "  +dayOfMonth+ "/" + (month + 1) + "/" + year, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER, 0, 0);
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Selected Date: " + dayOfMonth + "/" + (month + 1) + "/" + year, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
                 toast.show();
             }
         });
     }
 }
 
-//    d MMM yyyy
+
